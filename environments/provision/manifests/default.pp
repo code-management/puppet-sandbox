2a208e1987f346675e925aafd48e6647bda99a59
David Hayes <d.hayes@accenture.com>
2016-05-31 22:16:57 +0100

    Correct linting errors for relative class names

--- environments/provision/manifests/default.pp
+++ environments/provision/manifests/default.pp
@@ -3,14 +3,16 @@
 #
 
 # use run stages for minor vagrant environment fixes
-stage { 'pre': before => Stage['main'] }
+stage { '::pre': before => Stage['main'] }
 
-class { 'repos':   stage => 'pre' }
-class { 'vagrant': stage => 'pre' }
+class { '::repos':   stage => '::pre' }
+class { '::vagrant': stage => '::pre' }
 
-class { 'puppet': }
-class { 'networking': }
+class { '::puppet': }
+class { '::networking': }
 
 if $hostname == 'puppet' {
-  class { 'puppet::server': }
+  class { '::puppet::server':
+    before => Class['puppet'],
+  }
 }
